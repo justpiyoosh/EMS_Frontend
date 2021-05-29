9e910382e37928e82c143966cec701c4c0d1fa20
Saurav pandey <pandeysaurav355@gmail.com>
2021-05-29 16:45:22 +0530

    Posts

--- src/posts/Post.js
+++ src/posts/Post.js
@@ -1,8 +1,17 @@
+import Axios from 'axios';
 import React from 'react';
+var reply = "";
+function Repost(){
+    reply = document.getElementById("Reply").value;
+    Axios.post('http://localhost:8000/api/account/blog_post',{'content':reply});
+}
 const Post = (props) =>{
 return(
-<div style = {{border:"1px solid black"}}>
+<div style = {{border:"1px solid black",padding:"5px"}}>
     {props.item.msg}
+    <br/><br/>
+    <input type = "text" placeholder="Reply" id = "Reply"/>
+    <button type = "submit" onClick = {Repost}/>
 </div>);
 }
 export default Post;
